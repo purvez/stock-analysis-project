d44f14e020d0d6f21f672cee0ff90e39aaf49d62
Purvez <purvez32@gmail.com>
2025-09-26 22:15:35 +0800

    removed old files

--- README.md
+++
@@ -1,7 +0,0 @@
-To run this file.
-
-make sure u do "pip install streamlit plotly pandas numpy yfinance matplotlib" preferably on a virtual environment
-
-
-after that run "streamlit run streamlit_app.py". you can then see it on localhost.
-
--- validation.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from analysis import compute_sma
-
-def validate_sma():
-    test_prices = [10, 20, 30, 40, 50]
-    sma_manual = [None, None, 20.0, 30.0, 40.0]  # window=3
-    sma_computed = compute_sma(test_prices, 3)
-    return sma_manual == sma_computed
