9c5307737143932456e380b3af6cc086b2584aaa
aloysius-hub <2502850@sit.singaporetech.edu.sg>
2025-10-08 15:18:21 +0800

    Added capitlisation to the header of df table

--- data_cleaning.py
+++ data_cleaning.py
@@ -8,6 +8,7 @@ def clean_data(df: pd.DataFrame):
     3. Convert to '%m/%d/%Y' format
     4. Deleting rows with missing value
     5. Sort dateindex to ascending order
+    6. Only capitalise the first char of the column header
     
     Parameters
     df (pd.DataFrame): Raw stocks data frame
@@ -19,6 +20,7 @@ def clean_data(df: pd.DataFrame):
     df = df.dropna(subset=["Date"]) #Drop any date that cannot be converted to date time
     df = df.sort_values('Date').reset_index(drop=True) #Sort date by ascending order
     df = df.dropna() #Delete rows with any missing value
+    df.columns = df.columns.astype(str).str.strip().str.capitalize() #Captilise the first char of the column header, and the rest as lower char
 
 
     return df
